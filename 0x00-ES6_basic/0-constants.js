3f16b843b44e0871b6e0e5e016e9707e0c6aff65
Dickson-Onyongo <dicksononyong@gmail.com>
2024-10-01 21:19:28 +0000

    Const or let

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,13 @@
+export taskFirst = () => {
+	const task = 'I prefer const when I can.';
+	return task;
+}
+export getLast = () => ('is okay');
+
+export taskNext = () => {
+	let combination = 'But sometimes let';
+	combnation += getLast();
+
+	return combination;
+}
+
