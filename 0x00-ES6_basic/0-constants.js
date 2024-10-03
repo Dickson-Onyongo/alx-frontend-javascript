2c4f2ad1a46be7b552230b525e050e9a64edda43
Dickson-Onyongo <dicksononyong@gmail.com>
2024-10-03 22:08:09 +0300

    task 0 corrections, change of function structure

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,13 +1,14 @@
-export const taskFirst = () => {
+export function taskFirst() {
   const task = 'I prefer const when I can. ';
   return task;
-};
+}
 
-export const getLast = () => ('is okay');
-
-export const taskNext = () => {
+export function getLast() {
+  return 'is okay';
+}
+export function taskNext() {
   let combination = 'But sometimes let ';
   combination += getLast();
 
   return combination;
-};
+}
