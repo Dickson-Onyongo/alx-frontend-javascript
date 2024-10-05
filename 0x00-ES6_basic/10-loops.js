b81e669fae84e24f01ccf052fdf048a53ac3753b
Dickson-Onyongo <dicksononyong@gmail.com>
2024-10-05 13:50:36 +0300

    for of loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const newArray = array;
   for (const value of array) {
+    const idx = array.indexOf(value);
+    newArray[idx] = appendString + value;
+  }
 
-    array[array.indexOf(value)] = appendString + value;
-}
-
-return array;
+  return array;
 }
